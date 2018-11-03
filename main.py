ee710513f8363598d809383772248e5348b2247f
Vadim Kravtsov <mr.vadim.kravtsov@mail.ru>
2018-11-03 21:40:59 +0300

    added main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from scipy.integrate import quad 
+
+def beta(R, T, M, Q, a, p):
+	"""
+	Gravity force to light pressure force fractoin.
+
+	R - radius  (star)
+	T - temperature  (star)
+	M - mass  (star)
+	Q - Plank factor of light pressure
+	a - radius (dust)
+	p - density (dust)
+	"""
+	return 2.12*10e-8*R**2*T**4*Q/(M*a*p)
+
+print(beta(7*10e10, 6*10e3, 2*10e33, 1, 10e-4, 2.5))
+
